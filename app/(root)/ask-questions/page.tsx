bc4fc0e560e2b2ecb4432387917dd2287e811cb2
Joseph <jnakarmi123@gmail.com>
2023-09-24 20:05:10 -0400

    Auth setup

---
+++ app/(root)/ask-questions/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AskQueston = () => {
+  return <div>AskQueston</div>;
+};
+
+export default AskQueston;
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="h1-bold">Next JS 13...</h1>
-    </>
-  );
-}
